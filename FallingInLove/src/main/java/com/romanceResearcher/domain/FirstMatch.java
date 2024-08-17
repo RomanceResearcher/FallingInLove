480e701218d030c68e25a4d7c77ecd25ed8ccb93
wildcat222 <wildcat222@naver.com>
2024-08-18 00:40:55 +0900

    MatchService sendSignal.acceptFlagUpdate 구현중

--- FallingInLove/src/main/java/com/romanceResearcher/domain/FirstMatch.java
+++ FallingInLove/src/main/java/com/romanceResearcher/domain/FirstMatch.java
@@ -5,13 +5,15 @@ import java.time.LocalTime;
 
 public class FirstMatch implements Serializable {
 
-    private long firstMatchNo; // 매치 번호
+    private long firstMatchNo = 0; // 매치 번호
     private User to; // 호감 보낸 사람
     private User from; // 호감 받는 사람
     private boolean acceptFlag; // From 이 To 에게 호감을 보낸 여부 (true 라면 최종결정 db에 저장)
     private LocalTime createDate; // 매치 생성 일자
 
-    public FirstMatch(User to, User from, LocalTime createDate) {
+    // 단방향 호감 전송 기록 생성
+    public FirstMatch(long firstMatchNo, User to, User from, LocalTime createDate) {
+        this.firstMatchNo = firstMatchNo;
         this.to = to;
         this.from = from;
         this.createDate = createDate;
--- FallingInLove/src/main/java/com/romanceResearcher/repository/MatchRepository.java
+++ FallingInLove/src/main/java/com/romanceResearcher/repository/MatchRepository.java
@@ -3,10 +3,7 @@ package com.romanceResearcher.repository;
 import com.romanceResearcher.domain.FirstMatch;
 import com.romanceResearcher.domain.User;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,30 +13,65 @@ public class MatchRepository {
     // 매치 기록을 저장하는 리스트 선언 및 초기화
     private static final String FILENAME = "src/main/java/com/romanceResearcher/db/firstMatchRepo.dat";
     private final List<FirstMatch> firstMatches = new ArrayList<>();
+    private static MatchRepository instance;
 
+    public static MatchRepository getInstance() {
+        if (instance == null) {
+            instance = new MatchRepository();
+        }
+        return instance;
+    }
 
-    public MatchRepository() {
+    // MatchRepository 생성자
+    // 파일에서 읽어와서 firstMatches 에 저장
+    private UserRepository() {
 
         File file = new File(FILENAME);
 
         if (!file.exists()) {
+            FirstMatch sample01 = new FirstMatch(1,new User(),new User(), LocalTime.now());
+            FirstMatch sample02 = new FirstMatch(2,new User(),new User(), LocalTime.now());
+            FirstMatch sample03 = new FirstMatch(3,new User(),new User(), LocalTime.now());
 
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILENAME))) {
-                // sample user 추가
-                firstMatches.add(new FirstMatch());
-            } catch (IOException e) {
-                System.out.println("해당 파일이 존재하지 않습니다.");
-            }
-        } else {
-            loadUser(file);
+            firstMatches.add(sample01);
+            firstMatches.add(sample02);
+            firstMatches.add(sample03);
+
+            saveUser(file);
         }
+        loadUser(file);
 
-    // CRUD
-    private long firstMatchgityNo; // 매치 번호
-    private User to; // 호감 보낸 사람
-    private User from; // 호감 받는 사람
-    private boolean acceptFlag; // From 이 To 에게 호감을 보낸 여부 (true 라면 최종결정 db에 저장)
-    private LocalTime createDate; // 매치 생성 일자
+    }
+
+    // firstMatches 파일에서 데이터 꺼내오기
+    public void loadUser(File file) {
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            while (true) {
+                FirstMatch firstMatch = (FirstMatch) ois.readObject();
+            }
+
+        } catch (EOFException e) {
+            System.out.println("파일을 모두 로딩했습니다.");
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // firstMatches 파일에 데이터 저장하기
+    public void saveUser(File file) {
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            for (FirstMatch firstMatch : firstMatches) {
+                oos.writeObject(firstMatch);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 
     public MatchRepository(File) {}
 
