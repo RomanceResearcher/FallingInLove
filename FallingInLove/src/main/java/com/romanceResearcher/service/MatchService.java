2dfa816596bfa713f8e17af109c3665de8deea4f
wildcat222 <wildcat222@naver.com>
2024-08-18 21:45:29 +0900

    ..

--- FallingInLove/src/main/java/com/romanceResearcher/service/MatchService.java
+++ FallingInLove/src/main/java/com/romanceResearcher/service/MatchService.java
@@ -7,7 +7,6 @@ import com.romanceResearcher.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class MatchService {
